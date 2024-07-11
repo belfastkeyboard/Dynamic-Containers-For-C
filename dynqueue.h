d268cb80fea23ae464fbf593fb81afe1834b4f29
riain <ryanftoal@gmail.com>
2024-07-11 16:25:21 +0100

    Use calloc instead of malloc

--- dynqueue.h
+++ dynqueue.h
@@ -30,17 +30,22 @@
 
 #pragma once
 
+// CONSIDER: pop operations can be slow if many items need to be popped quickly
+//  due to having to shift the entire array down by an element every pop
+
+// CONSIDER: no ability to shrink allocated memory
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
 #include <assert.h>
 #include <stdbool.h>
 
-#define constructor_queue(type) {                                                                   \
-    ._elements = 0, ._capacity = 0, ._elem_size = sizeof(type), ._array = malloc(sizeof(type) * 0), \
-    .push_##type = queue_push_##type,                                                               \
-    .pop_##type = queue_pop_##type,                                                                 \
-    .front_##type = queue_front_##type, .back_##type = queue_back_##type,                           \
+#define constructor_queue(type) {                                                                 \
+    ._elements = 0, ._capacity = 0, ._elem_size = sizeof(type), ._array = calloc(0, sizeof(int)), \
+    .push_##type = queue_push_##type,                                                             \
+    .pop_##type = queue_pop_##type,                                                               \
+    .front_##type = queue_front_##type, .back_##type = queue_back_##type,                         \
     .empty_##type = queue_empty_##type, .size_##type = queue_size_##type }
 
 #ifndef destructor
@@ -65,22 +70,24 @@
     bool   (*empty_##type)(struct queue_##type*);                       \
     size_t (*size_##type)(struct queue_##type*);                        \
 } queue_##type;                                                         \
+                                                                        \
 void queue_push_##type(struct queue_##type* que, type elem)             \
 {                                                                       \
     if (que->_elements >= que->_capacity)                               \
     {                                                                   \
         size_t _capacity = que->_capacity;                              \
-        type *cpy = malloc(que->_elem_size * _capacity);                \
+        type *cpy = calloc(_capacity, que->_elem_size);                 \
         memcpy(cpy, que->_array, que->_elem_size * _capacity);          \
         free(que->_array);                                              \
         que->_capacity = (que->_capacity > 0) ? que->_capacity * 2 : 1; \
-        que->_array = malloc(que->_elem_size * que->_capacity);         \
+        que->_array = calloc(que->_capacity, que->_elem_size);          \
         memcpy(que->_array, cpy, que->_elem_size * _capacity);          \
         free(cpy);                                                      \
     }                                                                   \
     que->_array[que->_elements] = elem;                                 \
     que->_elements++;                                                   \
 }                                                                       \
+                                                                        \
 void queue_pop_##type(struct queue_##type* que)                         \
 {                                                                       \
     assert(que->_elements != 0);                                        \
@@ -90,20 +97,24 @@ void queue_pop_##type(struct queue_##type* que)                         \
     memmove(destination, source, amount);                               \
     que->_elements--;                                                   \
 }                                                                       \
+                                                                        \
 type queue_front_##type(struct queue_##type* que)                       \
 {                                                                       \
     assert(que->_elements > 0);                                         \
     return que->_array[0];                                              \
 }                                                                       \
+                                                                        \
 type queue_back_##type(struct queue_##type* que)                        \
 {                                                                       \
     assert(que->_elements > 0);                                         \
     return que->_array[que->_elements - 1];                             \
 }                                                                       \
+                                                                        \
 bool queue_empty_##type(struct queue_##type* que)                       \
 {                                                                       \
     return (que->_elements == 0);                                       \
 }                                                                       \
+                                                                        \
 size_t queue_size_##type(struct queue_##type* que)                      \
 {                                                                       \
     return que->_elements;                                              \
