6728c57d841abe75ca7ac9a87709b802d1b551fa
riain <ryanftoal@gmail.com>
2024-07-11 16:48:50 +0100

    Use calloc instead of malloc

--- dynarray.h
+++ dynarray.h
@@ -1,5 +1,7 @@
 // Dynamic array implementation in C based on C++ vector implementation
 
+// TODO: test
+
 /*  HOW TO USE:
 
     Call ARRAY(type) with the desired type, multiple types can be used.
@@ -37,7 +39,7 @@
 #include <stdbool.h>
 
 #define constructor_array(type) {                                                                                     \
-    ._elements = 0, ._capacity = 0, ._elem_size = sizeof(type), ._array = malloc(sizeof(type) * 0),                   \
+    ._elements = 0, ._capacity = 0, ._elem_size = sizeof(type), ._array = calloc(0, sizeof(int)),                     \
     .push_back_##type = array_push_##type, .insert_##type = array_insert_##type,                                      \
     .pop_back_##type = array_pop_back_##type, .erase_##type = array_erase_##type, .clear_##type = array_clear_##type, \
     .front_##type = array_front_##type, .back_##type = array_back_##type, .get_##type = array_get_##type,             \
@@ -72,32 +74,34 @@
     void   (*reserve_##type)(struct array_##type*, size_t);                \
     void   (*shrink_##type)(struct array_##type*);                         \
 } array_##type;                                                            \
+                                                                           \
 void array_push_##type(struct array_##type* arr, type elem)                \
 {                                                                          \
     if (arr->_elements >= arr->_capacity)                                  \
     {                                                                      \
         size_t _capacity = arr->_capacity;                                 \
-        type *cpy = malloc(arr->_elem_size * _capacity);                   \
+        type *cpy = calloc(_capacity, arr->_elem_size);                    \
         memcpy(cpy, arr->_array, arr->_elem_size * _capacity);             \
         free(arr->_array);                                                 \
         arr->_capacity = (arr->_capacity > 0) ? arr->_capacity * 2 : 1;    \
-        arr->_array = malloc(arr->_elem_size * arr->_capacity);            \
+        arr->_array = calloc(arr->_capacity, arr->_elem_size);             \
         memcpy(arr->_array, cpy, arr->_elem_size * _capacity);             \
         free(cpy);                                                         \
     }                                                                      \
     arr->_array[arr->_elements] = elem;                                    \
     arr->_elements++;                                                      \
 }                                                                          \
+                                                                           \
 void array_insert##type(struct array_##type* arr, type elem, size_t index) \
 {                                                                          \
     if (arr->_elements >= arr->_capacity)                                  \
     {                                                                      \
         size_t _capacity = arr->_capacity;                                 \
-        type *cpy = malloc(arr->_elem_size * _capacity);                   \
+        type *cpy = calloc(_capacity, arr->_elem_size);                    \
         memcpy(cpy, arr->_array, arr->_elem_size * _capacity);             \
         free(arr->_array);                                                 \
         arr->_capacity = (arr->_capacity > 0) ? arr->_capacity * 2 : 1;    \
-        arr->_array = malloc(arr->_elem_size * arr->_capacity);            \
+        arr->_array = calloc(arr->_capacity, arr->_elem_size);            \
         memcpy(arr->_array, cpy, arr->_elem_size * _capacity);             \
         free(cpy);                                                         \
     }                                                                      \
@@ -117,11 +121,13 @@ void array_insert##type(struct array_##type* arr, type elem, size_t index) \
         arr->_elements++;                                                  \
     }                                                                      \
 }                                                                          \
+                                                                           \
 void array_pop_##type(struct array_##type* arr)                            \
 {                                                                          \
     assert(arr->_elements > 0);                                            \
     arr->_elements--;                                                      \
 }                                                                          \
+                                                                           \
 size_t array_erase_##type(struct array_##type* arr, size_t index)          \
 {                                                                          \
     assert(index <= arr->_elements);                                       \
@@ -140,54 +146,62 @@ size_t array_erase_##type(struct array_##type* arr, size_t index)          \
         return index - 1;                                                  \
     }                                                                      \
 }                                                                          \
+                                                                           \
 type array_front_##type(struct array_##type* arr)                          \
 {                                                                          \
     assert(arr->_elements > 0);                                            \
     return arr->_array[0];                                                 \
 }                                                                          \
+                                                                           \
 type array_back_##type(struct array_##type* arr)                           \
 {                                                                          \
     assert(arr->_elements > 0);                                            \
     return arr->_array[arr->_elements - 1];                                \
 }                                                                          \
+                                                                           \
 type array_get_##type(struct array_##type* arr, size_t index)              \
 {                                                                          \
     assert(index < arr->_elements);                                        \
     return arr->_array[index];                                             \
 }                                                                          \
+                                                                           \
 bool array_empty_##type(struct array_##type* arr)                          \
 {                                                                          \
     return (arr->_elements == 0);                                          \
 }                                                                          \
+                                                                           \
 size_t array_size_##type(struct array_##type* arr)                         \
 {                                                                          \
     return arr->_elements;                                                 \
 }                                                                          \
+                                                                           \
 void array_clear_##type(struct array_##type* arr)                          \
 {                                                                          \
     free(arr->_array);                                                     \
     arr->_elements = 0;                                                    \
     arr->_capacity = 0;                                                    \
-    arr->_array = malloc(arr->_elem_size * arr->_capacity);                \
+    arr->_array = calloc(arr->_capacity, arr->_elem_size);                 \
 }                                                                          \
+                                                                           \
 void array_reserve_##type(struct array_##type* arr, size_t amount)         \
 {                                                                          \
     assert(amount > arr->_capacity);                                       \
-    type *cpy = malloc(arr->_elem_size * arr->_elements);                  \
+    type *cpy = calloc(arr->_elements, arr->_elem_size);                   \
     memcpy(cpy, arr->_array, arr->_elem_size * arr->_elements);            \
     free(arr->_array);                                                     \
-    arr->_array = malloc(arr->_elem_size * amount);                        \
+    arr->_array = calloc(amount, arr->_elem_size);                         \
     memcpy(arr->_array, cpy, arr->_elem_size * arr->_elements);            \
     free(cpy);                                                             \
     arr->_capacity = amount;                                               \
 }                                                                          \
+                                                                           \
 void array_shrink_##type(struct array_##type* arr)                         \
 {                                                                          \
     if (arr->_elements == arr->_capacity) return;                          \
-    type *cpy = malloc(arr->_elem_size * arr->_elements);                  \
+    type *cpy = calloc(arr->_elements, arr->_elem_size);                   \
     memcpy(cpy, arr->_array, arr->_elem_size * arr->_elements);            \
     free(arr->_array);                                                     \
-    arr->_array = malloc(arr->_elem_size * arr->_elements);                \
+    arr->_array = calloc(arr->_elements, arr->_elem_size);                 \
     memcpy(arr->_array, cpy, arr->_elem_size * arr->_elements);            \
     free(cpy);                                                             \
     arr->_capacity = arr->_elements;                                       \
